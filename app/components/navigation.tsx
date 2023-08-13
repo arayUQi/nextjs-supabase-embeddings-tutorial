cb56b5cd4aa2790ad0e32aa8ec6feb58f3c57ced
araUQi <yuki.araki@uq.net.au>
2023-08-13 17:43:22 +0900

    #3

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -8,7 +8,7 @@ const Navigation = () => {
     <header className="border-b py-5">
       <div className="text-center">
         <Link href="/" className="font-bold text-xl cursor-pointer">
-          FullStackChannel
+          ダイナモテックソリューション株式会社：機械学習班
         </Link>
       </div>
     </header>
