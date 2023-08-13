5df660fbbf786835db6f33694355fe1b729badd8
araUQi <yuki.araki@uq.net.au>
2023-08-13 17:50:34 +0900

    #4

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -8,7 +8,8 @@ const Navigation = () => {
     <header className="border-b py-5">
       <div className="text-center">
         <Link href="/" className="font-bold text-xl cursor-pointer">
-          ダイナモテックソリューション株式会社：機械学習班
+          ダイナモテックソリューション株式会社
+          <div>機械学習班</div>
         </Link>
       </div>
     </header>
